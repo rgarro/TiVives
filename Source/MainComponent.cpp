4f7259b18fdda6258c1fedcc0e84e5c0f6fea7d9
rgarro <rgarro@gmail.com>
2018-01-30 17:37:52 -0600

    私は戻ってきます
    私は戻ってきます

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,28 +1,16 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
+#ifndef MAINCOMPONENT_H_INCLUDED
+#define MAINCOMPONENT_H_INCLUDED
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
-//==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
+
 class MainContentComponent   : public AudioAppComponent
 {
 public:
-    //==============================================================================
     MainContentComponent()
     {
         setSize (800, 600);
-
-        // specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
+        setAudioChannels (0, 2); // no inputs, two outputs
     }
 
     ~MainContentComponent()
@@ -30,63 +18,41 @@ public:
         shutdownAudio();
     }
 
-    //==============================================================================
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
     {
-        // This function will be called when the audio device is started, or when
-        // its settings (i.e. sample rate, block size, etc) are changed.
-
-        // You can use this function to initialise any resources you might need,
-        // but be careful - it will be called on the audio thread, not the GUI thread.
-
-        // For more details, see the help for AudioProcessor::prepareToPlay()
-    }
-
-    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
-    {
-        // Your audio-processing code goes here!
-
-        // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-        // Right now we are not producing any data, in which case we need to clear the buffer
-        // (to prevent the output of random noise)
-        bufferToFill.clearActiveBufferRegion();
+        String message;
+        message << "Preparing to Play Audio ...\n";
+        message << " samplesPerBlockExpected = " << samplesPerBlockExpected << "\n";
+        message << " sampleRate = " << sampleRate;
+        Logger::getCurrentLogger()->writeToLog (message);
     }
 
     void releaseResources() override
     {
-        // This will be called when the audio device stops, or when it is being
-        // restarted due to a setting change.
-
-        // For more details, see the help for AudioProcessor::releaseResources()
+        Logger::getCurrentLogger()->writeToLog ("Releasing audio resources");
     }
-
-    //==============================================================================
-    void paint (Graphics& g) override
+    
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
     {
-        // (Our component is opaque, so we must completely fill the background with a solid colour)
-        g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
-        // You can add your drawing code here!
-    }
-
-    void resized() override
-    {
-        // This is called when the MainContentComponent is resized.
-        // If you add any child components, this is where you should
-        // update their positions.
+        for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
+        {
+            // Get a pointer to the start sample in the buffer for this audio output channel
+            float* const buffer = bufferToFill.buffer->getWritePointer (channel, bufferToFill.startSample);
+            
+            // Fill the required number of samples with noise betweem -0.125 and +0.125
+            for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
+                buffer[sample] = random.nextFloat() * 0.25f - 0.125f;
+        }
     }
 
 
 private:
-    //==============================================================================
-
-    // Your private member variables go here...
-
+    Random random;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
 };
 
-
-// (This function is called by the app startup code to create our main component)
 Component* createMainContentComponent()     { return new MainContentComponent(); }
+
+
+#endif  // MAINCOMPONENT_H_INCLUDED
